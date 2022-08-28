d74260a516480f2a05ad9fcb58a7f03c8e262c74
Marius Indreberg <mariusindreberg@Mariuss-MacBook-Pro.local>
2022-08-28 20:05:18 +0200

    removed garbage

---
+++ src/Server/Program.fs
@@ -0,0 +1,44 @@
+﻿open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open Giraffe
+open System
+
+
+
+let getRandomNum  next ctx = 
+    task {
+        let r = new Random()
+        return! Successful.OK (r.Next() % 100) next ctx 
+    }
+
+let webApp =
+    choose [
+        route "/ping"   >=> text "pong"
+        route "/"       >=> htmlFile "/pages/index.html"
+        route "/random" >=> getRandomNum  ]
+
+type Startup() =
+    member __.ConfigureServices (services : IServiceCollection) =
+        // Register default Giraffe dependencies
+        services.AddGiraffe() |> ignore
+
+    member __.Configure (app : IApplicationBuilder)
+                        (env : IHostEnvironment)
+                        (loggerFactory : ILoggerFactory) =
+        // Add Giraffe to the ASP.NET Core pipeline
+        app.UseGiraffe webApp
+
+[<EntryPoint>]
+let main _ =
+    Host.CreateDefaultBuilder()
+        .ConfigureWebHostDefaults(
+            fun webHostBuilder ->
+                webHostBuilder
+                    .UseStartup<Startup>()
+                    |> ignore)
+        .Build()
+        .Run()
+    0
